82b67b3518533b665467c1bd9f9b6c4439a111f2
Dm1tryb1t <afanasevdima465@gmail.com>
2024-05-19 16:53:29 +0300

    Stariy bog

---
+++ build/CMakeFiles/Singleton.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/dmitry/Документы/cpp_design_patterns/Patterns/Singleton/ConceptualExample01.cpp" "CMakeFiles/Singleton.dir/ConceptualExample01.cpp.o" "gcc" "CMakeFiles/Singleton.dir/ConceptualExample01.cpp.o.d"
+  "/home/dmitry/Документы/cpp_design_patterns/Patterns/Singleton/ConceptualExample02.cpp" "CMakeFiles/Singleton.dir/ConceptualExample02.cpp.o" "gcc" "CMakeFiles/Singleton.dir/ConceptualExample02.cpp.o.d"
+  "/home/dmitry/Документы/cpp_design_patterns/Patterns/Singleton/Program.cpp" "CMakeFiles/Singleton.dir/Program.cpp.o" "gcc" "CMakeFiles/Singleton.dir/Program.cpp.o.d"
+  "/home/dmitry/Документы/cpp_design_patterns/Patterns/Singleton/Singleton_vs_Dependency_Injection.cpp" "CMakeFiles/Singleton.dir/Singleton_vs_Dependency_Injection.cpp.o" "gcc" "CMakeFiles/Singleton.dir/Singleton_vs_Dependency_Injection.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Singleton.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Singleton.dir/ConceptualExample01.cpp.o"
+  "CMakeFiles/Singleton.dir/ConceptualExample01.cpp.o.d"
+  "CMakeFiles/Singleton.dir/ConceptualExample02.cpp.o"
+  "CMakeFiles/Singleton.dir/ConceptualExample02.cpp.o.d"
+  "CMakeFiles/Singleton.dir/Program.cpp.o"
+  "CMakeFiles/Singleton.dir/Program.cpp.o.d"
+  "CMakeFiles/Singleton.dir/Singleton_vs_Dependency_Injection.cpp.o"
+  "CMakeFiles/Singleton.dir/Singleton_vs_Dependency_Injection.cpp.o.d"
+  "Singleton"
+  "Singleton.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Singleton.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
